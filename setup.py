6347aec846041626c6b3fc2c0f1d772efa3297d3
Marcle Rodrigues <maarclee@gmail.com>
2017-09-16 17:53:22 -0300

    Adding basic strucuture to a python project

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='sorvent',
+      version='0.1',
+      description="Helps you to sort out teams in coding events, matching tutors and participants.",
+      url='',
+      author='Marcle Rodrigues',
+      author_email='maarclee@gmail.com',
+      license='MIT',
+      packages=['sorvent'],
+      zip_false=False
+     )
